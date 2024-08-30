57dc44a471e856e6664b38f9377e6734220b76e5
selsebil-ben <selsebilben.dev@gmail.com>
2024-08-31 00:15:37 +0200

    add new APIs

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
- 
+
+
 const express = require('express');
 const mongoose = require('mongoose');
-const Customer = require('./models/customer');
+//const allRoutes = require('./routes/allRoutes');
 const app = express();
-const port = 3001;
-require('dotenv').config();
+const port = process.env.PORT || 3001;
+require('dotenv').config(); // using .env
 const dbString = process.env.DATABASE_URL;
-var moment = require('moment');
 
+const bodyParser = require('body-parser');
+
+const Customer = require('./models/customer');
+const moment = require('moment'); // for adding timestamps in my DB schema
+
+
+// parse application/json
+// app.use(bodyParser.json())
+
+const methodOverride = require('method-override') // For delete function
+
+//app.use(allRoutes);
 app.use(express.urlencoded({extended : true}));
+
 app.use(express.json());
 app.use(express.static('public')) // public is a folder contains static files like css, js or images
+app.use(methodOverride('_method')) // For delete function
 
 app.set('view engine','ejs');
 
@@ -28,76 +42,103 @@ liveReloadServer.refresh("/");
 }, 100);
 });
 
-//---------------------------------
-
-
-
+//-----------DB connection ---------------
 mongoose.connect(dbString)
 .then( () => {console.log("Database successfuly conencted !")})
 .catch( (Error) => {console.log(Error)});
 
+
+
 //-----------------Get API 1 ---------------
 app.get('/', (req, res) => {
-Customer.find()
-.then( (result) => {res.render("index", {customers: result, moment : moment})
-  
-})
-.catch((err) => {console.log(err)
-  
-})
-})
-
-//-----------------Get API 2 ---------------
-app.get('/user/add', (req, res) => {
-  
-   res.render("user/add", {})})
- 
-//-----------------Get API 3 ---------------
-app.get('/success', (req, res) => {
-  
-  Customer.find()
-  .then( (result) => { res.render("user/addconf", {title : "Confirmation", cname : result})})
-  .catch( (err) => { console.log(err)})
+    Customer.find()
+    .then( (result) => {res.render("index", {customers: result, moment : moment})})
+    .catch((err) => {console.log(err)}) })
     
-})
-
-
- //-------- /!\ Warning Let API with variables (:id) at the end according to the reg ex :id is recognized as string like /add, /view------------- 
-  
-//----------------- Get API 4---------------
+    
+//-----------------Get API 1 ---------------
+app.get('/', (req, res) => {
+    Customer.find()
+    .then( (result) => {res.render("index", {customers: result, moment : moment})})
+    .catch((err) => {console.log(err)}) })
+    
+    
+    //-----------------Get API 2 ---------------
+app.get('/user/add', (req, res) => {
+      
+       res.render("user/add", {})})
+     
+    //-----------------Get API 3 ---------------
+app.get('/success', (req, res) => {
+      
+      Customer.find()
+      .then( (result) => { res.render("user/addconf", {title : "Confirmation", cname : result})})
+      .catch( (err) => { console.log(err)}) })
+        
+    
+    
+    
+    
+     //-------- /!\ Warning Let API with variables (:id) at the end according to the reg ex :id is recognized as string like /add, /view------------- 
+      
+    //----------------- Get API 4---------------
 app.get("/edit/:id", (req, res) => {
-  Customer.findById(req.params.id)
-    .then((result)=>{res.render("user/edit", {customer : result, moment : moment})})
-    .catch( (err) => {console.log(err)})
-})
-
-//----------------- Get API 6  --------------
- 
-  app.get("/view/:id", (req, res) => {
-    Customer.findById(req.params.id)
-    .then((result)=>{res.render("user/view", {customer : result, moment : moment})})
-    .catch( (err) => {console.log(err)})
-  
-  })
-
-
-//----------------- POST API--------------
+    
+      Customer.findById(req.params.id)
+        .then((result)=>{res.render("user/edit", {customer : result, moment : moment})}) 
+        .catch( (err) => {console.log(err)}) })
+    
+    
+    //----------------- Get API 5  --------------
+     
+app.get("/view/:id", (req, res) => {
+        Customer.findById(req.params.id)
+        .then((result)=>{res.render("user/view", {customer : result, moment : moment})})
+        .catch( (err) => {console.log(err)})  })
+      
+     
+    
+    
+    //----------------- POST API  1 --------------
 app.post("/user/add", (req, res) => {
- 
- const cust = new Customer(req.body);
- cust.save()
- .then( () => { res.redirect("/success")})
- .catch( (er) => {console.log(er)});
-})
+    console.log(req.body)
+     const cust = new Customer(req.body);
+     Customer.create(req.body) // DO NOT USE Customer.insertOne(req.body); A Mongoose model doesn't have an insertOne method. Use the create method instead:
+     .then( () => { res.redirect("/success")})
+     .catch( (er) => {console.log(er)}); 
+    })
+    
+    
+    //-----------------POST API 2 ---------------
+app.post('/search', (req, res) => {
+    
+      //console.log(req.body.keyword)
+      
+      Customer.find({$or: [{ firstName: { $regex: req.body.keyword, $options: "i" } }, { lastName: { $regex: req.body.keyword, $options: "i" } }]})
+      .then( (result) => { console.log(result); res.render("user/search", {customers : result, moment : moment})})
+      .catch( (err) => { console.log(err)}) })
+        
+//----------------- Update API--------------
+app.put("/update/:id", (req, res) => {
+     console.log(req.body)
+    Customer.updateOne({_id : req.params.id}, req.body).then(()=>{console.log("updated") ; res.redirect('/')})
+    .catch( (err) => {console.log(err)})})
+  
+    // or Customer.findByIdAndUpdate(req.params.id, req.body) 
+          
+    
+    //----------------- DELETE API--------------
+    // or Customer.findByIdAndDelete(req.params.id) 
+app.delete("/delete/:id", (req, res) => {
+    console.log('done')
+    Customer.deleteOne({_id : req.params.id})
+    .then(()=>{res.redirect('/')})
+    .catch( (err) => {console.log(err)}) })
 
 
-
-
-//res.render("user/view", {customer : result}) 66cf3007f2182609d5991c21
-console.log("hello")
 
 
 app.listen(port, () => {
-  
-  console.log(`http://localhost:${port}/`)
-});
+ 
+console.log("We are up and running, captain ! ")
+console.log(`http://localhost:${port}/`) });
