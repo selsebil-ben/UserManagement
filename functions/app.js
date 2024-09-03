a6b26d76152da68dcf2f5f97368acab51f72dc65
selsebil-ben <selsebilben.dev@gmail.com>
2024-09-03 14:37:49 +0200

    add a prefix to the APIs paths for production env

--- functions/app.js
+++ functions/app.js
@@ -50,28 +50,30 @@ mongoose.connect(dbString)
 .catch( (Error) => {console.log(Error)});
 
 
+// Function to get the base path dynamically
+const getBasePath = () => {
+    return process.env.NODE_ENV === 'production' ? '/.netlify/functions/api' : '';
+};
 
-//-----------------Get API 1 ---------------
-app.get('/', (req, res) => {
-    Customer.find()
-    .then( (result) => {res.render("index", {customers: result, moment : moment})})
-    .catch((err) => {console.log(err)}) })
+const basePath = getBasePath();
+
+
     
     
 //-----------------Get API 1 ---------------
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     Customer.find()
     .then( (result) => {res.render("index", {customers: result, moment : moment})})
     .catch((err) => {console.log(err)}) })
     
     
     //-----------------Get API 2 ---------------
-app.get('/user/add', (req, res) => {
+router.get('/user/add', (req, res) => {
       
        res.render("user/add", {})})
      
     //-----------------Get API 3 ---------------
-app.get('/success', (req, res) => {
+router.get('/success', (req, res) => {
       
       Customer.find()
       .then( (result) => { res.render("user/addconf", {title : "Confirmation", cname : result})})
@@ -84,7 +86,7 @@ app.get('/success', (req, res) => {
      //-------- /!\ Warning Let API with variables (:id) at the end according to the reg ex :id is recognized as string like /add, /view------------- 
       
     //----------------- Get API 4---------------
-app.get("/edit/:id", (req, res) => {
+router.get("/edit/:id", (req, res) => {
     
       Customer.findById(req.params.id)
         .then((result)=>{res.render("user/edit", {customer : result, moment : moment})}) 
@@ -93,7 +95,7 @@ app.get("/edit/:id", (req, res) => {
     
     //----------------- Get API 5  --------------
      
-app.get("/view/:id", (req, res) => {
+router.get("/view/:id", (req, res) => {
         Customer.findById(req.params.id)
         .then((result)=>{res.render("user/view", {customer : result, moment : moment})})
         .catch( (err) => {console.log(err)})  })
@@ -102,7 +104,7 @@ app.get("/view/:id", (req, res) => {
     
     
     //----------------- POST API  1 --------------
-app.post("/user/add", (req, res) => {
+router.post("/user/add", (req, res) => {
     
      const cust = new Customer(req.body);// u can use cust.save() res.redirect("/success")
      Customer.create(req.body) // DO NOT USE Customer.insertOne(req.body); A Mongoose model doesn't have an insertOne method. Use the create method instead:
@@ -112,7 +114,7 @@ app.post("/user/add", (req, res) => {
     
     
     //-----------------POST API 2 ---------------
-app.post('/search', (req, res) => {
+router.post('/search', (req, res) => {
     
       //console.log(req.body.keyword)
       
@@ -121,7 +123,7 @@ app.post('/search', (req, res) => {
       .catch( (err) => { console.log(err)}) })
         
 //----------------- Update API--------------
-app.put("/update/:id", (req, res) => {
+router.put("/update/:id", (req, res) => {
      console.log(req.body)
     Customer.updateOne({_id : req.params.id}, req.body).then(()=>{console.log("updated") ; res.redirect('/')})
     .catch( (err) => {console.log(err)})})
@@ -131,14 +133,13 @@ app.put("/update/:id", (req, res) => {
     
     //----------------- DELETE API--------------
     // or Customer.findByIdAndDelete(req.params.id) 
-app.delete("/delete/:id", (req, res) => {
+router.delete("/delete/:id", (req, res) => {
     console.log('done')
     Customer.deleteOne({_id : req.params.id})
     .then(()=>{res.redirect('/')})
     .catch( (err) => {console.log(err)}) })
 
-
-
+app.use( basePath,router); 
 
 app.listen(port, () => {
  
@@ -146,5 +147,6 @@ console.log("We are up and running, captain ! ")
 console.log(`http://localhost:${port}/`) });
 
 //app.use(allRoutes);
-// app.use('/.netlify/functions/api', router); 
-// module.exports.handler = serverless(app); run w 
+
+// Export the serverless handler for Netlify
+module.exports.handler = serverless(app); 
